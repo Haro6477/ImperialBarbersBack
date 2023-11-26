a8076ab4e8a64671bbcfde456cbfdd13fac10b55
Omar Viveros <jaro64.77@gmail.com>
2023-11-25 19:25:34 -0600

    error export const

--- config.js
+++ config.js
@@ -1,7 +1,16 @@
-export const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 
-export const DB_HOST = process.env.DB_HOST || "localhost"
-export const DB_USER = process.env.DB_USER || "root"
-export const DB_PASSWORD = process.env.DB_PASSWORD || "Oneway64.77"
-export const DB_NAME = process.env.DB_NAME || "db_kingBarberApp"
-export const DB_PORT = process.env.DB_PORT || null
+const DB_HOST = process.env.DB_HOST || "localhost"
+const DB_USER = process.env.DB_USER || "root"
+const DB_PASSWORD = process.env.DB_PASSWORD || "Oneway64.77"
+const DB_NAME = process.env.DB_NAME || "db_kingBarberApp"
+const DB_PORT = process.env.DB_PORT || null
+
+module.exports = {
+  PORT: PORT,
+  DB_HOST: DB_HOST,
+  DB_USER: DB_USER,
+  DB_PASSWORD: DB_PASSWORD,
+  DB_NAME: DB_NAME,
+  DB_PORT: DB_PORT
+};
