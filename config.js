8f7218e8a176cd8491a2cf2424df20f319d33f49
Haro6477 <jaro64.77@gmail.com>
2024-11-01 13:45:29 -0600

    exports fix

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 require('dotenv').config();
 
-const DB_HOST = process.env.DATABASE_HOST || "localhost"
-const DB_USER = process.env.DATABASE_USER || "root"
-const DB_PASSWORD = process.env.DATABASE_PASSWORD || "Oneway64.77"
-const DB_NAME = process.env.DATABASE_NAME || "db_kingBarberApp"
-const DB_PORT = process.env.DB_PORT || 5432
+const DB_HOST = process.env.DATABASE_HOST || "localhost";
+const DB_USER = process.env.DATABASE_USER || "root";
+const DB_PASSWORD = process.env.DATABASE_PASSWORD || "Oneway64.77";
+const DB_NAME = process.env.DATABASE_NAME || "db_kingBarberApp";
+const DB_PORT = process.env.DB_PORT || 5432;
 
-// Cargar las variables de entorno
-
-export const host = DB_HOST;
-export const username = DB_USER;
-export const password = DB_PASSWORD;
-export const database = DB_NAME;
-export const port = DB_PORT;
+module.exports = {
+  host: DB_HOST,
+  username: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_NAME,
+  port: DB_PORT
+};
