0cfbf7ed7d4b0ac6d94468bdfbe694b0d5bedb52
Haro6477 <113865622+Haro6477@users.noreply.github.com>
2024-12-13 13:42:03 -0600

    fix ::date

--- index.js
+++ index.js
@@ -996,7 +996,7 @@ app.get("/cobros-hoy/municipio/:municipio", async (req, res) => {
             INNER JOIN clientes AS c ON v.idCliente = c.id
             INNER JOIN empleados AS b ON v.idBarber = b.id
             INNER JOIN empleados AS s ON v.idCobrador = s.id
-            WHERE v.fecha = (CURRENT_TIMESTAMP AT TIME ZONE 'America/Mexico_City')::date AND v.municipio = ${municipio}
+            WHERE v.fecha = CURRENT_TIMESTAMP AT TIME ZONE 'America/Mexico_City' AND v.municipio = ${municipio}
             ORDER BY fecha DESC
         `;
         res.send(result);
@@ -1148,7 +1148,7 @@ app.get("/movimientos/municipio/:municipio", async (req, res) => {
             SELECT m.id, concepto, cantidad, fechaHora, nombre, m.municipio 
             FROM movimientos AS m 
             INNER JOIN empleados ON idUsuario = empleados.id 
-            WHERE DATE(v.fecha AT TIME ZONE 'UTC' AT TIME ZONE 'America/Mexico_City') != CURRENT_DATE AT TIME ZONE 'America/Mexico_City' AND m.municipio = ${municipio}
+            WHERE m.fechaHora != CURRENT_TIMESTAMP AT TIME ZONE 'America/Mexico_City' AND m.municipio = ${municipio}
         `;
         res.send(result);
     } catch (err) {
@@ -1274,7 +1274,7 @@ app.get("/reporte-hoy/municipio/:municipio", async (req, res) => {
         const result = await sql`
             SELECT id 
             FROM reportes 
-            WHERE fecha = (CURRENT_TIMESTAMP AT TIME ZONE 'America/Mexico_City')::date AND municipio = ${municipio}
+            WHERE fecha = CURRENT_TIMESTAMP AT TIME ZONE 'America/Mexico_City' AND municipio = ${municipio}
         `;
         res.send(result[0]);
     } catch (err) {
