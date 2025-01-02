02228e2af73d37fb64c12f22ee1584d14fbed84c
Haro6477 <113865622+Haro6477@users.noreply.github.com>
2025-01-02 17:34:38 -0600

    similarity 0.1

--- index.js
+++ index.js
@@ -815,7 +815,7 @@ app.get("/servicio/:id", async (req, res) => {
 app.get("/servicios/search/municipio/:municipio", async (req, res) => {
     const municipio = req.params.municipio;
     const text = `%${req.query.text}%`;
-    const similarityScore = 0.2;
+    const similarityScore = 0.1;
 
     try {
         const result = await sql`
@@ -835,7 +835,7 @@ app.get("/servicios/search/municipio/:municipio", async (req, res) => {
 app.get("/productos/search/municipio/:municipio", async (req, res) => {
     const municipio = req.params.municipio;
     const text = `%${req.query.text}%`;
-    const similarityScore = 0.2;
+    const similarityScore = 0.1;
 
     try {
         const result = await sql`
